1ee9c69fcab9317611e959d0048d6d0a94021f72
al-martyn1 <amart@mail.ru>
2025-05-17 17:25:20 +0300

    Начал накидывать

--- types.h
+++ types.h
@@ -37,7 +37,81 @@ namespace expressions {
       в структуру, в которой также храним знак.
       Вообще, хорошо бы сделать отдельный тип BigInt, по типу Decimal, но пока обойдусь
 
+    - Число с плавающей точкой - используем marty::Decimal
+
+    - Строковый литерал - храним в виде строки
+
+    - Символьный литерал - храним в виде строки
+
+    - Идентификатор - имя или список имён - список имён получается из последовательности идентификаторов,
+      разделённых NS-разделителем.
+
+    - Оператор. Оператор задаётся токеном оператора. Тип токена - шаблонный.
+
+    - Скобка. Скобка - тоже оператор. Открывающая, закрывающая - разные оператор-токены.
+      Надо уметь по типу токена оператора понимать, что это скобка, понимать - открывающая или нет (isBrace, isOpen).
+
+    Что ещё?
+
 */
+
+template<typename PositionInfoType, typename UnsignedType=std::uint64_t>
+struct IntegerLiteral
+{
+    PositionInfoType     positionInfo;
+
+    UnsignedType         value;
+    bool                 negSign; // if true, value is negative
+
+    bool isNegative() const { return negSign; }
+
+}; // IntegerLiteral
+
+
+template<typename PositionInfoType, typename FloatingPointType=marty::Decimal>
+struct FloatingPointLiteral
+{
+    PositionInfoType     positionInfo;
+
+    FloatingPointType    value;
+
+    bool isNegative() const { return value.sign()<0; }
+
+}; // FloatingPointLiteral
+
+
+template<typename PositionInfoType, typename StringType>
+struct StringLiteral
+{
+    PositionInfoType     positionInfo;
+
+    StringType           value;
+
+}; // StringLiteral
+
+
+template<typename PositionInfoType, typename StringType>
+struct SymbolLiteral
+{
+    PositionInfoType     positionInfo;
+
+    StringType           value;
+
+}; // SymbolLiteral
+
+
+template<typename PositionInfoType, typename OperatorTokenType>
+struct OperatorToken
+{
+    PositionInfoType     positionInfo;
+
+    OperatorTokenType    value;
+
+}; // SymbolLiteral
+
+
+
+
 
 
 /*
